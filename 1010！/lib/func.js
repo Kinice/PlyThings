8c90e771a73c2a92a6b28ecf186733df64c3f154
Kinice <szp93@126.com>
2016-08-22 22:08:44 +0800

    模块化

--- 1010！/lib/func.js
+++ 1010！/lib/func.js
@@ -1,7 +1,6 @@
-function Block(line_amount,color){ //块构造函数
-	this.line_amount = line_amount;
+//constructor
+function Block(color){ //block constructor
 	this.color = color;
-	this.id = id;
 	
 	if(typeof this.createBlock !='function'){
 		Block.prototype.createBlock = function(id,top,left,size){
@@ -15,9 +14,66 @@ function Block(line_amount,color){ //块构造函数
 			if(id){
 				block.setAttribute('id',id);
 			}
+
 			block.style.top = top + 'px';
 			block.style.left = left + 'px';
 			return block;
 		}
 	}
+}
+
+//functions
+function initGame(element,gameMatrix,columns,color){//initialize
+	for(var i = 0; i < columns; i++){
+		var temp = [];
+		for(var j = 0; j < columns; j++){
+			temp.push(0);
+		}
+		gameMatrix.push(temp);
+	}
+	appendBackBlocks(element,columns,color);
+}
+
+function appendBackBlocks(element,columns,color){//append game container
+	for(var i = 0; i < columns; i++){
+		for(var j = 0; j < columns; j++){
+			var id = i+'-'+j;
+			var t = i*50;
+			var l = j*50;
+			element.appendChild(color.createBlock(id,t,l,'big'));
+		}
+	}
+}
+
+function appendBlocks(element,matrix,color){//append new blocks
+	//set parent node's size for position
+	element.style.height = matrix.length * 30 + 'px';
+	element.style.width  = matrix[0].length * 30 + 'px';
+
+	//append block
+	for(var m = 0; m < matrix.length; m++){
+		var t = m*30;
+		if(matrix[m]!=undefined){
+			for(var n = 0; n < matrix[m].length; n++){
+				var l = n*30;
+				var id = 'n3-'+m+'-'+n;
+				if(matrix[m][n] == 1){
+					element.appendChild(color.createBlock(id,t,l,'small'));
+				}else{
+					element.appendChild(tranBlock.createBlock(id,t,l,'small'));
+				}
+			}
+		}else{
+			var id = 'n3-'+m+'one'
+			element.appendChild(color.createBlock(id,0,0,'small'));
+		}
+	}
+}
+
+function startDrag(){
+
+}
+
+function stopDrag(){
+
 }
--- 1010！/lib/main.js
+++ 1010！/lib/main.js
@@ -1,18 +1,21 @@
-var block = new Block(100,'grey');
-var bd = document.getElementById('bd');
-var container = document.getElementById('container');
-var newb = document.getElementById('newb');
+//blocks
+var backBlock = new Block('grey'),
+	redBlock  = new Block('dred'),
+	blueBlock = new Block('dblue'),
+	origBlock = new Block('loringe'),
+	tranBlock = new Block('');
+//elements
+var bd = document.getElementById('bd'),
+    container = document.getElementById('container'),
+    newb1 = document.getElementById('newb1'),
+    newb2 = document.getElementById('newb2'),
+    newb3 = document.getElementById('newb3');
+//game matrix
+var GAME = [];
 
-for(var i = 0; i < 10; i++){
-	for(var j = 0; j < 10; j++){
-		var id = i+'-'+j;
-		var t = i*50;
-		var l = j*50;
-		container.appendChild(block.createBlock(id,t,l,'big'));
-	}
-}
+//functions
+initGame(container,GAME,10,backBlock);
 
-for(var k = 0; k < 5; k++){
-	var l = k*30;
-	newb.appendChild(block.createBlock(k,0,l,'small'))
-}
+appendBlocks(newb1,matrix.row5,redBlock);
+appendBlocks(newb2,matrix.all3,blueBlock);
+appendBlocks(newb3,matrix.bl2,origBlock);
