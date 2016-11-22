adcecda82ab701b538a9b1d8731b5b234f841734
Kinice <szp93@126.com>
2016-11-22 16:28:17 +0800

    .

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -11,25 +11,19 @@ module.exports = {
     },
     output: { 
         path: path.join(__dirname, 'dist'), 
-        publicPath: './dist/',              
+        publicPath: '/dist/',              
         filename: 'js/[name].js',           
         chunkFilename: 'js/[id].chunk.js'  
     },
     module: {
-        loaders: [ //加载器，关于各个加载器的参数配置，可自行搜索之。
+        loaders: [ 
             {
                 test: /\.css$/,
-                //配置css的抽取器、加载器。'-loader'可以省去
                 loader: ExtractTextPlugin.extract('style-loader', 'css-loader') 
             }, {
                 test: /\.less$/,
-                //配置less的抽取器、加载器。中间!有必要解释一下，
-                //根据从右到左的顺序依次调用less、css加载器，前一个的输出是后一个的输入
-                //你也可以开发自己的loader哟。有关loader的写法可自行谷歌之。
                 loader: ExtractTextPlugin.extract('css!less')
             }, {
-                //html模板加载器，可以处理引用的静态资源，默认配置参数attrs=img:src，处理图片的src引用的资源
-                //比如你配置，attrs=img:src img:data-src就可以一并处理data-src引用的资源了，就像下面这样
                 test: /\.html$/,
                 loader: "html?attrs=img:src img:data-src"
             }, {
@@ -42,50 +36,49 @@ module.exports = {
         ]
     },
     plugins: [
-        new webpack.ProvidePlugin({ //加载jq
+        new webpack.ProvidePlugin({ 
             $: 'jquery'
         }),
         new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendors', // 将公共模块提取，生成名为`vendors`的chunk
-            chunks: ['index','about'], //提取哪些模块共有的部分
-            minChunks: 3 // 提取至少3个模块共有的部分
+            name: 'vendors', 
+            chunks: ['index','about'], 
+            minChunks: 2
         }),
-        new ExtractTextPlugin('css/[name].css'), //单独使用link标签加载css并设置路径，相对于output配置中的publickPath
+        new ExtractTextPlugin('css/[name].css'), 
 
-        //HtmlWebpackPlugin，模板生成相关的配置，每个对于一个页面的配置，有几个写几个
-        new HtmlWebpackPlugin({ //根据模板插入css/js等生成最终HTML
-            favicon: './src/img/favicon.ico', //favicon路径，通过webpack引入同时可以生成hash值
-            filename: './view/index.html', //生成的html存放路径，相对于path
-            template: './src/view/index.html', //html模板路径
-            inject: 'body', //js插入的位置，true/'head'/'body'/false
-            hash: true, //为静态资源生成hash值
-            chunks: ['vendors', 'index'],//需要引入的chunk，不配置就会引入所有页面的资源
-            minify: { //压缩HTML文件    
-                removeComments: true, //移除HTML中的注释
-                collapseWhitespace: false //删除空白符与换行符
+        new HtmlWebpackPlugin({ 
+            favicon: './src/img/favicon.ico', 
+            filename: './view/index.html', 
+            template: './src/view/index.html', 
+            inject: 'body', 
+            hash: true, 
+            chunks: ['vendors', 'index'],
+            minify: {   
+                removeComments: true, 
+                collapseWhitespace: false 
             }
         }),
-        new HtmlWebpackPlugin({ //根据模板插入css/js等生成最终HTML
-            favicon: './src/img/favicon.ico', //favicon路径，通过webpack引入同时可以生成hash值
-            filename: './view/about.html', //生成的html存放路径，相对于path
-            template: './src/view/about.html', //html模板路径
-            inject: true, //js插入的位置，true/'head'/'body'/false
-            hash: true, //为静态资源生成hash值
-            chunks: ['vendors', 'about'],//需要引入的chunk，不配置就会引入所有页面的资源
-            minify: { //压缩HTML文件    
-                removeComments: true, //移除HTML中的注释
-                collapseWhitespace: false //删除空白符与换行符
+        new HtmlWebpackPlugin({ 
+            favicon: './src/img/favicon.ico', 
+            filename: './view/about.html', 
+            template: './src/view/about.html', 
+            inject: true, 
+            hash: true,
+            chunks: ['vendors', 'about'],
+            minify: { 
+                removeComments: true, 
+                collapseWhitespace: false 
             }
         }),
 
-        new webpack.HotModuleReplacementPlugin() //热加载
+        new webpack.HotModuleReplacementPlugin()
     ],
-    //使用webpack-dev-server，提高开发效率
+   
     devServer: {
         contentBase: './',
         host: 'localhost',
-        port: 9090, //默认8080
-        inline: true, //可以监控js变化
-        hot: true, //热启动
+        port: 9090, 
+        inline: true, 
+        hot: true, 
     }
 };
