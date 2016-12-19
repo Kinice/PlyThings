6f947783fe4d8c0402804dd4ddf6c48e7ee38cf1
Kinice <szp93@126.com>
2016-12-19 18:22:40 +0800

    :alien:

---
+++ dozens/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div class="g-header">
+      
+    </div>
+    <div class="g-sidebar">
+      
+    </div>
+    <div class="g-main">
+      
+    </div>
+    <div class="g-footer">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+@test = #000;
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#app .g-header{
+  height: 100px;
+  width: 100%;
+  background: red;
+}
+</style>
