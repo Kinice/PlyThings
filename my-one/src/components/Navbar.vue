89973b58534f831e28322df853e09280c8da8e27
Kinice <szp93@126.com>
2017-01-12 19:47:24 +0800

    .

--- my-one/src/components/Navbar.vue
+++ my-one/src/components/Navbar.vue
@@ -1,10 +1,15 @@
 <template>
   <header class="g-hd">
     <div class="m-logo">
-      
+      <span>Kinice SPA</span>
     </div>
     <div class="m-user">
-      
+      <div class="m-wel">
+        欢迎您：<span>管理员</span>
+      </div>
+      <div class="m-exit">
+        <a href="#">退出</a>
+      </div>
     </div>
   </header>
 </template>
@@ -19,5 +24,24 @@
   .g-hd{
     height: 70px;
     background: #324057;
+    min-width: 875px;
+    .m-logo{
+      line-height: 70px;
+      margin-left: 70px; 
+      //替换
+      span{
+        color: #fff;
+        font-size: 2.2em;
+      }
+    }
+    .m-user{
+      .m-wel{
+
+      }
+      .m-exit{
+
+      }
+    }
   }
+
 </style>
