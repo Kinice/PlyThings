3cf724a76f548847129cb3ba5358bd957116e225
Kinice <szp93@126.com>
2017-03-07 16:12:17 +0800

    add weixinPage new change and manage background

--- weixinPage/script/handler.js
+++ weixinPage/script/handler.js
@@ -19,7 +19,38 @@
 
     //confirm event handler
     $('#confirm').on('click',function(e){
-        var code = $('#discountCode').val();
-        window.location.href='./result.html'+'?code='+code+'&status=ok';
+        var code = $('#discountCode').val(),
+            apiValidate = '/api/1.0/coupon/validate',
+            url = './result.html?code='+code+'&status=';
+
+        var d = {
+            'coupon_code':code
+        }
+
+        var finalData = JSON.stringify(d);
+
+        $.ajax(apiValidate,{
+            'data': finalData,
+            'type': 'POST',
+            'processData': false,
+            'contentType': 'application/json',
+            'success': function(data){
+                console.log(data)
+                if(data.request_result.code == 0){
+                    if(data.data.coupon){
+                        if(data.data.info==''){
+                            url += 'ok'+'&school='+data.data.coupon.school_code+'&verify='+data.data.coupon.verify_code+'&info=ok';
+                        }else{
+                            url += 'ok'+'&school='+data.data.coupon.school_code+'&verify='+data.data.coupon.verify_code+'&info=err';
+                        }
+                    }else{
+                        url += 'err';
+                    }
+                }else{
+                    url += 'err';
+                }
+                window.location.href=url;
+            }
+        })
     })
 })();
