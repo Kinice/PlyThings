e775fd3ab8d693c6a0c6b254927705ff6ecfc9b2
Kinice <szp93@126.com>
2017-03-08 18:11:24 +0800

    Change weixin page to new look

--- weixinPage/script/handler.js
+++ weixinPage/script/handler.js
@@ -1,56 +1,139 @@
 (function(){
-    //click event handler
-    $('.flex-container').on('click',function(e){
-        var thisHiddenItem = $($(this).find('.hidden-item')[0]),
-            elseHiddenItem = $($(this).siblings('.flex-container').find('.hidden-item')[0]);
+    //Common state define
+    var form = {
+        coupon_code: '',
+        name: '',
+        phone: '',
+        verify_code: ''
+    }
+    var state = {
+        coupon_code: 0,
+        name: 0,
+        phone: 0,
+        verify_code: 0
+    }
+    var reg = {
+        coupon_code: /^[a-z0-9A-Z]{10}$/,
+        name: /^[a-zA-Z\u4e00-\u9fa5]{1,20}$/,
+        phone: /^1\d{10}$/,
+        verify_code: /^\d{6}$/
+    }
+    var isSentVcode = false;
+    var isChangePhone = false;
+    var isCounting = false,
+        countDown = 60,
+        t;
+    var signupApi = '/api/1.0//signup',
+        vcodeApi = '/api/1.0/send_vcode';
 
-        var thisStatus = thisHiddenItem[0].style.display,
-            elseStatus = elseHiddenItem[0].style.display;
+    var url = './result.html';
 
-        if(!thisStatus||thisStatus == 'none'){
-            thisHiddenItem.slideDown(200);
-            if(elseStatus == 'block'){
-                elseHiddenItem.slideUp(200);
+    var setTime = function(val){//vcode count
+        if(isCounting){
+            val.attr('disabled','disabled');
+            val.addClass('weui-btn_disabled');
+            val.html('剩余'+countDown+'秒');
+            countDown--;
+            if(countDown<0){
+                isCounting = false;
+                countDown = 60;
             }
         }else{
+            val.removeAttr('disabled');
+            val.removeClass('weui-btn_disabled');
+            val.html('发送验证码');
+            clearTimeout(t);
             return false;
         }
-    });
-
-    //confirm event handler
+        t = setTimeout(function(){
+            setTime(val);
+        },1050);
+    }
+    //send vcode event
+    $('#sendVcode').on('click',function(e){
+        var phone = $('#phone').val();
+        if(reg['phone'].test(phone)){
+            var data = JSON.stringify({
+                'phone': phone
+            })
+            $.ajax(vcodeApi,{
+                'data': data,
+                'type': 'POST',
+                'processData': false,
+                'contentType': 'application/json',
+                'success': function(d){
+                    if(d.request_result.code == 0){
+                        weui.toast('发送成功',2000);
+                        isCounting = true;
+                        setTime($('#sendVcode'));
+                        isSentVcode = true;
+                        isChangePhone = false;
+                    }else{
+                        weui.alert(d.request_result.display_message)
+                    }    
+                },
+                'error': function(){
+                    weui.alert('发送失败！请检查网络环境是否良好')
+                }
+            });
+        }else{
+            weui.alert('请输入正确的手机号');
+            return false;
+        }
+    })
+    //Change phone after send vcode event
+    $('#phone').on('keyup',function(){
+        if(isSentVcode){
+            isChangePhone = true;
+        }
+    })
+    //Confirm event
     $('#confirm').on('click',function(e){
-        var code = $('#discountCode').val(),
-            apiValidate = '/api/1.0/coupon/validate',
-            url = './result.html?code='+code+'&status=';
-
-        var d = {
-            'coupon_code':code
+        var allStates = 0;
+        //get form and verify form with reg
+        for(var i in form){
+            form[i] = $('#'+i).val();
+            if(reg[i].test(form[i])){
+                state[i] = 1;
+                allStates += 1;
+            }
+            else state[i] = 0;
         }
 
-        var finalData = JSON.stringify(d);
-
-        $.ajax(apiValidate,{
-            'data': finalData,
-            'type': 'POST',
-            'processData': false,
-            'contentType': 'application/json',
-            'success': function(data){
-                console.log(data)
-                if(data.request_result.code == 0){
-                    if(data.data.coupon){
-                        if(data.data.info==''){
-                            url += 'ok'+'&school='+data.data.coupon.school_code+'&verify='+data.data.coupon.verify_code+'&info=ok';
-                        }else{
-                            url += 'ok'+'&school='+data.data.coupon.school_code+'&verify='+data.data.coupon.verify_code+'&info=err';
-                        }
+        if(allStates == 4 && !isChangePhone && isSentVcode){
+            form.coupon_code = form.coupon_code.toUpperCase();
+            $.ajax(signupApi,{
+                'data': JSON.stringify(form),
+                'type': 'POST',
+                'processData': false,
+                'contentType': 'application/json',
+                'success': function(d){
+                    if(d.request_result.code == 0){
+                        window.location.href = url;
                     }else{
-                        url += 'err';
+                        weui.alert(d.request_result.display_message);
                     }
-                }else{
-                    url += 'err';
+                },
+                'error': function(){
+                    weui.alert('验证失败！请检查网络环境是否良好')
                 }
-                window.location.href=url;
+            });
+        }else{
+            if(isChangePhone || !isSentVcode){
+                weui.alert('请输入手机号对应的验证码')
+            }else{
+                for(var i in state){
+                    if(state[i]==0){
+                        switch(i){
+                            case 'coupon_code': weui.alert('请输入正确的邀请码');break;
+                            case 'name': weui.alert('请输入正确的姓名');break;
+                            case 'phone': weui.alert('请输入正确的手机号');break;
+                            case 'verify_code': weui.alert('请输入正确的验证码');break;
+                            default: weui.alert('错误！请重新打开页面');break;
+                        }
+                    }
+                }
             }
-        })
+        }
     })
 })();
