db8fd3a87f79068b721ce8dc879c58748b68864c
Kinice <szp93@126.com>
2017-03-15 12:13:19 +0800

    disorder .js

---
+++ disorder.js
@@ -0,0 +1,17 @@
+var disorder = arr => {
+    let length = arr.length
+
+    while(length){
+        let rand = Math.floor(Math.random()*length),
+            t = arr[rand]
+
+        arr[rand] = arr[length-1]
+
+        arr[length-1] = t
+
+        length--
+        console.log(arr)
+    }
+
+    return arr;
+}
