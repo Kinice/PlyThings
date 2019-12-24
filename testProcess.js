da55c4d7763dd840090603690f0d63c1b6d879b9
Kinice <szp93@126.com>
2019-12-24 16:30:10 +0800

    add test process code.

---
+++ testProcess.js
@@ -0,0 +1,31 @@
+console.log('main1')
+
+setTimeout(() => {
+  console.log('settimeout')
+  process.nextTick(() => {
+    console.log('next tick2')
+  })
+}, 0)
+
+process.nextTick(() => {                                                                                                                                                             
+  console.log('next tick1')
+})
+
+setTimeout(() => {
+  console.log('setTimeout2')
+  process.nextTick(() => {
+    console.log('next tick3')
+  })
+})
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve()
+  })
+  console.log('promise1')
+}).then(() => {
+  console.log('promise2')
+})
+
+console.log('main2')
+
